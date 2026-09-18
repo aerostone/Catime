f23bf649c5c190bd96986b2103011fb072a388f6
zgc <zgc@l450.local>
2026-09-18 21:00:23 +0800

    fix(todo): fold sticky menu cmds into window TU (cross-TU static caused C2065)

--- src/todo/todo_stickies_window.c
+++ src/todo/todo_stickies_window.c
@@ -94,7 +94,22 @@ static void OpenEdit(StickyWin *sw) {
 }
 
 static void TodoSticky_RunMenuCommand(HWND hwnd, const char *taskId,
-                                      BOOL done, UINT cmd);
+                                      BOOL done, UINT cmd) {
+    StickyWin *sw = FindByHwnd(hwnd);
+    if (!sw || !taskId) return;
+    if (cmd == STICKY_MENU_DONE) {
+        TodoStore_SetDone(taskId, !done);
+        InvalidateRect(hwnd, NULL, TRUE);
+    } else if (cmd == STICKY_MENU_UNPIN) {
+        TodoSticky_SetPinned(taskId, FALSE);
+    } else if (cmd == STICKY_MENU_DELETE) {
+        char id[TODO_STORE_ID_LEN];
+        strcpy_s(id, sizeof(id), taskId);
+        TodoStickies_Forget(id);
+        TodoStore_Remove(id);
+    }
+}
+
 
 static LRESULT CALLBACK StickyProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     StickyWin *sw = FindByHwnd(hwnd);
