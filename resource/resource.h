8a51ab79369da33585339aa2f2e334de56b59b37
zgc <zgc@l450.local>
2026-09-18 17:03:46 +0800

    chore(release): bump CATIME_VERSION to 1.6.3

--- resource/resource.h
+++ resource/resource.h
@@ -6,7 +6,7 @@
 #ifndef CATIME_RESOURCE_H
 #define CATIME_RESOURCE_H
 
-#define CATIME_VERSION "1.6.2"
+#define CATIME_VERSION "1.6.3"
 
 /* Kept in the aggregate header for the embedded-resource packaging tool. */
 #define IDR_COMPRESSED_ASSETS 1200
